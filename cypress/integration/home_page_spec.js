d4eefef992c9731190f45a387e5a0a5abfbc90a4
jpasikainen <asikainen.juhapekka@gmail.com>
2022-01-27 14:50:36 +0200

    More cypress tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,7 +1,30 @@
-describe('The Home Page', () => {
+describe('Initial test', () => {
   it('successfully loads', () => {
-      cy.visit('/');
-      cy.get('h1').should('contain', 'Hylkusukellusilmoituspalvelu');
+    cy.visit("/")
+    cy.get('h1').should('contain', 'Hylkusukellusilmoituspalvelu');
   });
 });
   
+describe('Test form', () => {
+  it('user has to fill in their name', () => {
+    cy.get('.btn').click();
+    cy.get('[id=newname]').then($x => expect($x[0].checkValidity()).to.be.false);
+  });
+
+  it('user has to fill in their phone number', () => {
+    cy.get('[id=newname]').type('Test Tester');
+    cy.get('.btn').click();
+    cy.get('[id=newphone]').then($x => expect($x[0].checkValidity()).to.be.false);
+  });
+
+  it('user has to fill in the location', () => {
+    cy.get('[id=newphone]').type('0000000000');
+    cy.get('.btn').click();
+    cy.get('[id=newlocationname]').then($x => expect($x[0].checkValidity()).to.be.false);
+  });
+
+  it('user can make make a notice', () => {
+    cy.get('[id=newlocationname]').type('Test Tester');
+    cy.get('.btn').click();
+  });
+})
