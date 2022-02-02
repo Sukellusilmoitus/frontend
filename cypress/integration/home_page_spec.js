a2917fbf9e507fce993519f4c45b363e9da0a41e
Skorp7 <sanna.korpi@live.fi>
2022-02-02 18:22:26 +0200

    cypress test for notification form -fix

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -6,46 +6,51 @@ describe('Initial test', () => {
 });
   
 describe('Test form', () => {
+  it('locationid is empty before clicked a row', () => {
+    cy.get('[id=newlocationid]').should('have.value', '');
+  });
+
+  it('locationname is not empty when clicked a row', () => {
+    cy.wait(5000);
+    cy.scrollTo('top');
+    cy.wait(2000);
+    cy.get('table tbody').find('tr').first().click();
+    cy.get('[id=newlocationname]').should('not.have.value', '')
+  });
+
+  it('locationid is not empty when clicked a row', () => {
+    cy.wait(6000);
+    cy.scrollTo('top');
+    cy.wait(2000);
+    cy.get('table tbody').find('tr').first().click();
+    cy.get('[id=newlocationid]').should('not.have.value', '')
+  });
+
   it('user has to fill in their name', () => {
     cy.get('.btn').click();
+    cy.wait(2000);
     cy.get('[id=newname]').then($x => expect($x[0].checkValidity()).to.be.false);
   });
 
   it('user has to fill in their phone number', () => {
     cy.get('[id=newname]').type('Test Tester');
     cy.get('.btn').click();
+    cy.wait(2000);
     cy.get('[id=newphone]').then($x => expect($x[0].checkValidity()).to.be.false);
   });
 
-  it('user can make make a notice', () => {
-    cy.get('[id=newname]').type('Test Tester');
-    cy.get('[id=newphone]').type('0000000000');
-    cy.get('.btn').click();
-  });
-
-  it('locationid is empty before clicked a row', () => {
-    cy.get('[id=newlocationid]').should('have.value', '')
-  });
-
-  it('locationname is not empty when clicked a row', () => {
-    cy.scrollTo('top')
-    cy.get('table tbody').find('tr').first().click();
-    cy.get('[id=newlocationname]').should('not.have.value', '')
-  });
-
-  it('locationid is not empty when clicked a row', () => {
-    cy.scrollTo('top')
-    cy.get('table tbody').find('tr').first().click();
-    cy.get('[id=newlocationid]').should('not.have.value', '')
-  });
-
   it('locationname cannot be modified by typing', () => {
-    cy.get('[id=newlocationname]').type('Wreck 2', { force: true })
-    cy.get('[id=newlocationname]').should('not.have.value', 'Wreck 2')
+    cy.get('[id=newlocationname]').type('Wreck 2', { force: true });
+    cy.get('[id=newlocationname]').should('not.have.value', 'Wreck 2');
   });
 
   it('locationid cannot be modified by typing', () => {
-    cy.get('[id=newlocationid]').type('000000', { force: true })
-    cy.get('[id=newlocationid]').should('not.have.value', '000000')
+    cy.get('[id=newlocationid]').type('000000', { force: true });
+    cy.get('[id=newlocationid]').should('not.have.value', '000000');
   });
+
+  // it('user can make make a notice', () => {
+  //   cy.get('[id=newphone]').type('0000000000');
+  //   cy.get('.btn').click();
+  // });
 })
