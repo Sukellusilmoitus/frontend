5edb962a35c284f777ce24412bd4b4fbf7b060a0
jpasikainen <asikainen.juhapekka@gmail.com>
2022-02-11 14:35:59 +0200

    Clicking wreck directs to separate page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom';
 import Header from './components/Navigation';
 import ListAndNotification from './components/ListAndNotification';
+import NotificationForm from './components/NotificationForm';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Switch>
           <Route path="/hylyt" component={ListAndNotification} />
+          <Route path="/sukellusilmoitus/:id" component={NotificationForm} />
         </Switch>
       </Router>
     </div>
--- src/components/WrecksList.jsx
+++ src/components/WrecksList.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Spinner } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import wreckService from '../services/wrecks';
 
-function WrecksList(props) {
-  const { onRowClick } = props;
+function WrecksList() {
   const [wrecks, setWrecks] = useState('loading...');
 
   async function getWreckData() {
@@ -38,15 +38,14 @@ function WrecksList(props) {
             </thead>
             <tbody>
               {wrecks.features.map((wreck) => (
-                <tr
-                  key={wreck.properties.id}
-                  onClick={() => onRowClick(wreck.properties.name, wreck.properties.id)}
-                >
-                  <td>{wreck.properties.name}</td>
-                  <td>{wreck.properties.town}</td>
-                  <td>{wreck.properties.type}</td>
-                  <td><a href={wreck.properties.url}>{wreck.properties.source}</a></td>
-                </tr>
+                <LinkContainer to={`/sukellusilmoitus/${wreck.properties.id}`}>
+                  <tr key={wreck.properties.id}>
+                    <td>{wreck.properties.name}</td>
+                    <td>{wreck.properties.town}</td>
+                    <td>{wreck.properties.type}</td>
+                    <td><a href={wreck.properties.url}>{wreck.properties.source}</a></td>
+                  </tr>
+                </LinkContainer>
               ))}
             </tbody>
           </Table>
