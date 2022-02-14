fd8597561e16708209fd4b777b7b11978d326781
jpasikainen <asikainen.juhapekka@gmail.com>
2022-02-14 15:20:02 +0200

    Add back await

--- src/components/WrecksList.jsx
+++ src/components/WrecksList.jsx
@@ -7,7 +7,7 @@ function WrecksList(props) {
   const [wrecks, setWrecks] = useState('loading...');
 
   async function getWreckData() {
-    const data = wreckService.getAllWrecks();
+    const data = await wreckService.getAllWrecks();
     data.features.sort((a, b) => (a.properties.name > b.properties.name ? 1 : -1));
     setWrecks(data);
   }
