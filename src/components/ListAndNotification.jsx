443ed14eb9f7ef5ee4cb4f6c95da8f1f9859b943
jpasikainen <asikainen.juhapekka@gmail.com>
2022-02-15 19:23:49 +0200

    Implement click redirect

--- src/components/ListAndNotification.jsx
+++ src/components/ListAndNotification.jsx
@@ -1,49 +1,15 @@
-import React, { useState, useRef } from 'react';
-import NotificationForm from './NotificationForm';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import TargetsList from './TargetsList';
-import diveService from '../services/dives';
 
 function ListAndNotification() {
-  const [targetName, setTargetName] = useState('');
-  const [targetId, setTargetId] = useState('');
-  const [targetXcoordinate, setTargetXcoordinate] = useState('');
-  const [targetYcoordinate, setTargetYcoordinate] = useState('');
-  const formRef = useRef(null);
-
-  const scrollToBottom = () => {
-    formRef.current.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  const handleClick = (name, id, coordinates) => {
-    setTargetName(name);
-    setTargetId(id);
-    setTargetXcoordinate(coordinates[0]);
-    setTargetYcoordinate(coordinates[1]);
-    scrollToBottom();
-  };
-
-  const createNewNotification = (notification) => {
-    diveService.create(notification);
+  const history = useHistory();
+  const handleClick = (id) => {
+    history.push(`/hylyt/${id}`);
   };
 
   return (
-    <div>
-      <TargetsList onRowClick={handleClick} />
-      <div
-        style={{
-          float: 'left',
-          clear: 'both',
-        }}
-        ref={formRef}
-      />
-      <NotificationForm
-        targetName={targetName}
-        targetId={targetId}
-        targetXcoordinate={targetXcoordinate}
-        targetYcoordinate={targetYcoordinate}
-        createNotification={createNewNotification}
-      />
-    </div>
+    <TargetsList onRowClick={handleClick} />
   );
 }
 
