ba794c78614cac495ed9cc24e9726331ca6285e0
Skorp7 <sanna.korpi@live.fi>
2022-02-17 20:32:10 +0200

    form cypress tests updating

--- cypress/integration/dive_notification_page_spec.js
+++ cypress/integration/dive_notification_page_spec.js
@@ -1,38 +1,54 @@
-describe('Wreck can be clicked', () => {
-    it('successfully loads', () => {
-      cy.visit("/hylyt");
-      cy.get('table tbody').find('tr').first().click();
-      cy.contains('Tee uusi sukellusilmoitus');
-    });
+describe('Target can be clicked', () => {
+  it('successfully loads', () => {
+    cy.visit("/hylyt");
+    cy.get('table tbody').find('tr').first().click();
+    cy.contains('Tee uusi sukellusilmoitus');
   });
+});
   
-  describe('Test form', () => {
-    /*it('locationid is empty before clicked a row', () => {
-      cy.get('[id=newlocationid]').should('have.value', '');
-    });
-    
-    it('locationname is not empty when clicked a row', () => {
-      cy.wait(5000);
-      cy.scrollTo('top');
-      cy.wait(2000);
-      cy.get('table tbody').find('tr').first().click();
-      cy.get('[id=newlocationname]').should('not.have.value', '')
-    });*/
-    
-    it('user has to fill in their name', () => {
-      cy.get('.btn').click();
-      cy.get('[id=newname]').then($x => expect($x[0].checkValidity()).to.be.false);
-    });
-    
-    it('user has to fill in their phone number if email is empty', () => {
-      cy.get('[id=newname]').type('Test Tester');
-      cy.get('.btn').click();
-      cy.get('[id=newphone]').then($x => expect($x[0].checkValidity()).to.be.false);
-    });
-    
-    it('user has to fill in their email if phone number is not given', () => {
-      cy.get('.btn').click();
-      cy.get('[id=newemail]').then($x => expect($x[0].checkValidity()).to.be.false);
-    });
+describe('Test form fields', () => {
+  it('locationname is not empty on target page', () => {
+    cy.get('[id=newlocationname]').should('not.have.value', '')
   });
-  
+
+  it('locationid is not empty on target page', () => {
+    cy.get('[id=newlocationid]').should('not.have.value', '')
+  });
+    
+  it('user has to fill in their name', () => {
+    cy.get('.btn').click();
+    cy.get('[id=newname]').then($x => expect($x[0].checkValidity()).to.be.false);
+  });
+    
+  it('user has to fill in their phone number if email is empty', () => {
+    cy.get('[id=newname]').type('Test Tester');
+    cy.get('.btn').click();
+    cy.get('[id=newphone]').then($x => expect($x[0].checkValidity()).to.be.false);
+  });
+    
+  it('user has to fill in their email if phone number is not given', () => {
+    cy.get('.btn').click();
+    cy.get('[id=newemail]').then($x => expect($x[0].checkValidity()).to.be.false);
+  });
+
+  it('user has not to fill their phone number if email is given', () => {
+    cy.get('[id=newemail]').type('Test@Tester.com');
+    cy.get('[id=newphone]').type('000');
+    cy.get('[id=newphone]').clear();
+    cy.get('[id=newemail]').then($x => expect($x[0].checkValidity()).to.be.true);
+    cy.get('[id=newphone]').then($x => expect($x[0].checkValidity()).to.be.true);
+  });
+
+  it('user has not to fill their email if phone number is given', () => {
+    cy.get('[id=newemail]').clear();
+    cy.get('[id=newphone]').type('0001234567');
+    cy.get('[id=newemail]').then($x => expect($x[0].checkValidity()).to.be.true);
+    cy.get('[id=newphone]').then($x => expect($x[0].checkValidity()).to.be.true);
+  });
+
+  it('new coordinates are required if answered they were not correct', () => {
+    cy.get('[data-testid=testradio]').click();
+    cy.get('[id=newxcoordinate]').then($x => expect($x[0].checkValidity()).to.be.false);
+    cy.get('[id=newycoordinate]').then($x => expect($x[0].checkValidity()).to.be.false);
+  });
+});
