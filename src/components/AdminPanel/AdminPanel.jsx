37dcd51582fa8bc4f5c4d3c15954b4de5be781b4
Shmuli02 <samuli.kaleva@gmail.com>
2022-03-09 15:52:47 +0200

    targetlist edit view

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,14 +1,14 @@
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import TargetList from './targets';
-import UserList from './users';
+import { TargetListView, TargetListEdit } from './targets';
+import { UserListView, UserListEdit } from './users';
 
 const dataProvider = jsonServerProvider('http://127.0.0.1:5000/api/admin');
 export default function AdminPanel() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="targets" list={TargetList} />
-      <Resource name="users" list={UserList} />
+      <Resource name="targets" list={TargetListView} edit={TargetListEdit} />
+      <Resource name="users" list={UserListView} edit={UserListEdit} />
     </Admin>
   );
 }
--- src/components/AdminPanel/targets.jsx
+++ src/components/AdminPanel/targets.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import {
+  List,
+  Datagrid,
+  TextField,
+  Edit,
+  SimpleForm,
+  TextInput,
+  EditButton,
+} from 'react-admin';
 
-export default function TargetList(props) {
+function TargetListView(props) {
   return (
     <List {...props}>
       <Datagrid rowClick="edit">
@@ -10,7 +18,25 @@ export default function TargetList(props) {
         <TextField source="name" />
         <TextField source="town" />
         <TextField source="source" />
+        <EditButton />
       </Datagrid>
     </List>
   );
 }
+function TargetListEdit(props) {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput source="name" />
+        <TextInput source="town" />
+        <TextInput source="location_method" />
+        <TextInput source="location_accuracy" />
+        <TextInput source="url" />
+        <TextInput source="source" />
+        <TextInput source="coordinates" />
+      </SimpleForm>
+    </Edit>
+  );
+}
+
+export { TargetListView, TargetListEdit };
