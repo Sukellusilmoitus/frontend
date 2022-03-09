68f2b93e34b79ccc2f92af82c3d405e7cea278e4
Shmuli02 <samuli.kaleva@gmail.com>
2022-03-09 15:53:14 +0200

    userlist edit view

--- src/components/AdminPanel/users.jsx
+++ src/components/AdminPanel/users.jsx
@@ -1,16 +1,41 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import {
+  List,
+  Datagrid,
+  TextField,
+  EmailField,
+  Edit,
+  SimpleForm,
+  TextInput,
+  EditButton,
+} from 'react-admin';
 
-export default function UserList(props) {
+function UserListView(props) {
   return (
     <List {...props}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
         <TextField source="name" />
-        <TextField source="email" />
+        <EmailField source="email" />
         <TextField source="phone" />
+        <EditButton />
       </Datagrid>
     </List>
   );
 }
+
+function UserListEdit(props) {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput source="name" />
+        <TextInput source="town" />
+        <TextInput source="email" />
+        <TextInput source="phone" />
+      </SimpleForm>
+    </Edit>
+  );
+}
+
+export { UserListView, UserListEdit };
