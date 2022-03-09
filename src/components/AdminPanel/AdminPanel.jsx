c6c1a35b19aa264bf4a3c614444871f8070aa0f8
Shmuli02 <samuli.kaleva@gmail.com>
2022-03-09 15:06:33 +0200

    Add adminpanel users table

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,12 +1,14 @@
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import TargetList from './targets';
+import UserList from './users';
 
 const dataProvider = jsonServerProvider('http://127.0.0.1:5000/api/admin');
 export default function AdminPanel() {
   return (
     <Admin dataProvider={dataProvider}>
       <Resource name="targets" list={TargetList} />
+      <Resource name="users" list={UserList} />
     </Admin>
   );
 }
--- src/components/AdminPanel/users.jsx
+++ src/components/AdminPanel/users.jsx
@@ -2,14 +2,14 @@
 import * as React from 'react';
 import { List, Datagrid, TextField } from 'react-admin';
 
-export default function TargetList(props) {
+export default function UserList(props) {
   return (
     <List {...props}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
         <TextField source="name" />
-        <TextField source="town" />
-        <TextField source="source" />
+        <TextField source="email" />
+        <TextField source="phone" />
       </Datagrid>
     </List>
   );
