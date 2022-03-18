35e4ba5d71d33e6246fd2265c5174264bbe40ad8
Shmuli02 <samuli.kaleva@gmail.com>
2022-03-18 12:38:54 +0200

    adminpanel duplicats page
    Co-authored-by: jpasikainen <asikainen.juhapekka@gmail.com>

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -6,15 +6,17 @@ import { TargetListView, TargetListEdit } from './targets';
 import { UserListView, UserListEdit } from './users';
 import { DiveListView, DiveListEdit } from './dives';
 import { PendingListView, PendingListEdit } from './pending';
+import DuplicatesListView from './duplicates';
 
 const dataProvider = jsonServerProvider('http://127.0.0.1:5000/api/admin');
 export default function AdminPanel() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="targets" list={TargetListView} edit={TargetListEdit} icon={PostIcon} />
-      <Resource name="users" list={UserListView} edit={UserListEdit} icon={UserIcon} />
-      <Resource name="dives" list={DiveListView} edit={DiveListEdit} />
-      <Resource name="pending" list={PendingListView} edit={PendingListEdit} />
+      <Resource name="targets" options={{ label: 'Hylyt' }} list={TargetListView} edit={TargetListEdit} icon={PostIcon} />
+      <Resource name="users" options={{ label: 'Käyttäjät' }} list={UserListView} edit={UserListEdit} icon={UserIcon} />
+      <Resource name="dives" options={{ label: 'Sukellusilmoitukset' }} list={DiveListView} edit={DiveListEdit} />
+      <Resource name="pending" options={{ label: 'Hyväksyttävät' }} list={PendingListView} edit={PendingListEdit} />
+      <Resource name="duplicates" options={{ label: 'Päällekkäiset ilmoitukset' }} list={DuplicatesListView} />
     </Admin>
   );
 }
