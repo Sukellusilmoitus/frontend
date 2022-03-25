7bf0d5a84ca97fc5c8b75450ae55756b679c3857
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-25 13:25:45 +0200

    lint fix

--- src/components/AdminPanel/ReactAdmin.jsx
+++ src/components/AdminPanel/ReactAdmin.jsx
@@ -1,6 +1,5 @@
 import { Admin } from 'react-admin';
 import getAllTargets from '../../services/targets';
-import ThemeProvider from '@material-ui/styles'
 
 export default function AdminPanel() {
   return (
