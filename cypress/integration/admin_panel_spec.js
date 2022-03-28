63fdcc8f13a69a4c11a7a85c9cdb57bf3df22420
Shmuli02 <samuli.kaleva@gmail.com>
2022-03-28 16:02:21 +0300

    adminpanel target page testing

--- cypress/integration/admin_panel_spec.js
+++ cypress/integration/admin_panel_spec.js
@@ -4,6 +4,48 @@ const sort_arrow_desc = '.MuiTableSortLabel-root.MuiTableSortLabel-active.MuiTab
 const toString = (cells$) => _.map(cells$, 'textContent')
 const toNumbers = (id) => _.map(id,Number)
 
+function change_amount_of_rows_per_page (amount) {
+  cy.get('.MuiTablePagination-input').click()
+  cy.get('.MuiMenu-list').find(`[data-value=${amount}]`).click()
+  cy.get('table').within(() => {
+    cy.get("td:nth-child(2)").should('have.length',amount)
+  })
+}
+
+function sort_table_by (title,sortby_data,td_col,order) {
+  cy.get('table').within(() => {
+    if (order == 'Asc') {
+      cy.get("th").contains(title).click()
+      cy.contains("th", title)
+      .find(sort_arrow_asc).should('be.visible')
+      cy.wait(1000)
+      cy.get(td_col)
+      .then(toNumbers)
+      .then(toString)
+      .then((sortby_data) => {
+        const sorted = _.sortBy(sortby_data).reverse()
+        expect(sortby_data, 'sorted').to.deep.equal(sorted)
+      })
+    } else {
+      cy.get("th").contains(title).click()
+      cy.get("th").contains(title).click()
+      cy.contains("th", title)
+      .find(sort_arrow_desc).should('be.visible')
+
+      cy.wait(1000)
+      cy.get(td_col)
+      .then(toNumbers)
+      .then(toString)
+      .then((sortby_data) => {
+        const sorted = _.sortBy(sortby_data)
+        expect(sortby_data, 'sorted').to.deep.equal(sorted)
+      })
+    }
+
+
+  })
+}
+
 describe('Admin panel', () => {
   context('Target page', () => {
     beforeEach(() => {
@@ -12,152 +54,62 @@ describe('Admin panel', () => {
     it('deault 10 targets per page', () => {
       cy.wait(1000);
       cy.get('table').within(() => {
-        const ids = [];
-        cy.get("td:nth-child(2)").each(($e1, index, $list) => {
-          ids.push($e1.text());
-          // console.log(ids)
-        })
         cy.get("td:nth-child(2)").should('have.length',10)
         
       })})
-    it('sorts by name Asc', () => {
-      cy.get('table').within(() => {
-      cy.get("th").contains("Name").click()
-      cy.contains("th", "Name")
-      .find(sort_arrow_asc).should('be.visible')
-      
-      
-      cy.wait(1000)
-      cy.get('td:nth-child(3)')
-      .then(toString)
-      .then((name) => {
-        const sorted = _.sortBy(name).reverse()
-        expect(name, 'sorted').to.deep.equal(sorted)
+      it('5 target per page', () => {
+        change_amount_of_rows_per_page(5)
       })
+    it('25 target per page', () => {
+      change_amount_of_rows_per_page(25)
     })
+    it('sorts by id Asc', () => {
+      sort_table_by('Id','id','td:nth-child(2)','Asc')
+    })
+    it('sorts by id Desc', () => {
+      sort_table_by('Id','id','td:nth-child(2)','Desc')
+    })
+    it('sorts by name Asc', () => {
+      sort_table_by('Name','name','td:nth-child(3)','Asc')
     })
     it('sorts by name Desc', () => {
-      cy.get('table').within(() => {
-      cy.get("th").contains("Name").click()
-      cy.get("th").contains("Name").click()
-      cy.contains("th", "Name")
-      .find(sort_arrow_desc).should('be.visible')
-      
-      cy.wait(1000)
-      cy.get('td:nth-child(3)')
-      .then(toString)
-      .then((name) => {
-        const sorted = _.sortBy(name)
-        expect(name, 'sorted').to.deep.equal(sorted)
-      })
+      sort_table_by('Name','name','td:nth-child(3)','Desc')
     })
-  })
-  it('sorts by id Asc', () => {
-    cy.get('table').within(() => {
-    cy.get("th").contains("Id").click()
-    cy.contains("th", "Id")
-    .find(sort_arrow_asc).should('be.visible')
-    
-    cy.wait(1000)
-    cy.get('td:nth-child(2)')
-    .then(toNumbers)
-    .then((id) => {
-      const sorted = _.sortBy(id).reverse()
-      expect(id, 'sorted').to.deep.equal(sorted)
+    it('sorts by town Asc', () => {
+      sort_table_by('Town','town','td:nth-child(4)','Asc')
     })
-  })
-})
-it('sorts by id Desc', () => {
-  cy.get('table').within(() => {
-  cy.get("th").contains("Id").click()
-  cy.get("th").contains("Id").click()
-  cy.contains("th", "Id")
-  .find(sort_arrow_desc).should('be.visible')
-  
-  cy.wait(1000)
-  cy.get('td:nth-child(2)')
-  .then(toNumbers)
-  .then((id) => {
-    const sorted = _.sortBy(id)
-    expect(id, 'sorted').to.deep.equal(sorted)
-  })
-})
-})
-it('sorts by town Asc', () => {
-  cy.get('table').within(() => {
-  cy.get("th").contains("Town").click()
-  cy.contains("th", "Town")
-  .find(sort_arrow_asc).should('be.visible')
-  
-  cy.wait(1000)
-  cy.get('td:nth-child(4)')
-  .then(toString)
-  .then((town) => {
-    const sorted = _.sortBy(town).reverse()
-    expect(town, 'sorted').to.deep.equal(sorted)
-  })
-})
-})
-it('sorts by town Desc', () => {
-  cy.get('table').within(() => {
-  cy.get("th").contains("Town").click()
-  cy.get("th").contains("Town").click()
-  cy.contains("th", "Town")
-  .find(sort_arrow_desc).should('be.visible')
-  
-  cy.wait(1000)
-  cy.get('td:nth-child(4)')
-  .then(toString)
-  .then((town) => {
-    const sorted = _.sortBy(town)
-    
-    expect(town, 'sorted').to.deep.equal(sorted)
-  })
-})
-})
-///
-it('sorts by source Asc', () => {
-  cy.get('table').within(() => {
-  cy.get("th").contains("Source").click()
-  cy.contains("th", "Source")
-  .find(sort_arrow_asc).should('be.visible')
-  
-  cy.wait(1000)
-  cy.get('td:nth-child(5)')
-  .then(toString)
-  .then((source) => {
-    const sorted = _.sortBy(source).reverse()
-    expect(source, 'sorted').to.deep.equal(sorted)
-  })
-})
-})
-it('sorts by source Desc', () => {
-  cy.get('table').within(() => {
-  cy.get("th").contains("Source").click()
-  cy.get("th").contains("Source").click()
-  cy.contains("th", "Source")
-  .find(sort_arrow_desc).should('be.visible')
-  
-  cy.wait(1000)
-  cy.get('td:nth-child(5)')
-  .then(toString)
-  .then((source) => {
-    const sorted = _.sortBy(source)
-    expect(source, 'sorted').to.deep.equal(sorted)
-  })
-})
-})
+    it('sorts by town Desc', () => {
+      sort_table_by('Town','town','td:nth-child(4)','Desc')
+    })
+    it('sorts by source Asc', () => {
+      sort_table_by('Source','source','td:nth-child(5)','Asc')
+    })
+    it('sorts by source Desc', () => {
+      sort_table_by('Source','source','td:nth-child(5)','Desc')
+    })
   })
   context('User page', () => {
+    beforeEach(() => {
+      cy.visit('/admin#/users');
+    });
 
   })
   context('dives page', () => {
+    beforeEach(() => {
+      cy.visit('/admin#/dives');
+    });
 
   })
-  context('pending targets', () => {
+  context('pending targets page', () => {
+    beforeEach(() => {
+      cy.visit('/admin#/pending');
+    });
 
   })
-  context('overlap targets page', () => {
+  context('duplicates targets page', () => {
+    beforeEach(() => {
+      cy.visit('/admin#/duplicates');
+    });
 
   })
 })
