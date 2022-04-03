aa76d354e2637cfd58a890af68574ef35996adc4
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-04-03 10:30:07 +0300

    Add info message on submit success

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,9 +1,11 @@
-import { Container } from 'react-bootstrap';
+import { Container, Alert } from 'react-bootstrap';
+import { useState } from 'react';
 import FeedbackForm from './FeedbackForm';
 import PageTitle from '../PageTitle';
 import feedbackService from '../../services/feedbacks';
 
 function Feedback() {
+  const [message, setMessage] = useState('');
   const onSubmit = (feedback) => {
     const newFeedback = {
       feedback_text: feedback.feedback,
@@ -11,12 +13,21 @@ function Feedback() {
       feedback_giver_email: feedback.email,
       feedback_giver_phone: feedback.phone,
     };
-    feedbackService.create(newFeedback);
+    try {
+      feedbackService.create(newFeedback);
+      setMessage('Palaute lähetetty');
+      setTimeout(() => {
+        setMessage('');
+      }, 5000);
+    } catch (err) {
+      setMessage('Jotain meni pieleen');
+    }
   };
 
   return (
     <Container fluid>
       <PageTitle text="Anna palautetta" />
+      {message && <Alert variant="success">{message}</Alert>}
       <FeedbackForm onSubmit={onSubmit} />
     </Container>
   );
