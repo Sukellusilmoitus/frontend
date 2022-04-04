0d4938ca4d83d5a7a344d509cb7c9acbbd615258
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-04-04 09:51:50 +0300

    Fix to match new backend

--- src/services/feedbacks.js
+++ src/services/feedbacks.js
@@ -4,12 +4,12 @@ import REACT_APP_SERVER_URL from '../util/config';
 const baseUrl = REACT_APP_SERVER_URL;
 
 const getAll = async () => {
-  const response = await axios.get(`${baseUrl}/api/feedback`);
+  const response = await axios.get(`${baseUrl}/api/feedback/`);
   return response.data;
 };
 
 const create = async (newFeedback) => {
-  const response = await axios.post(`${baseUrl}/api/feedback`, newFeedback);
+  const response = await axios.post(`${baseUrl}/api/feedback/`, newFeedback);
   return response.data;
 };
 
