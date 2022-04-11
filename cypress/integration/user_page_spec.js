ffdd6296f5a26ff133f440884193f1bdfc1956c2
jpasikainen <asikainen.juhapekka@gmail.com>
2022-04-11 10:12:15 +0300

    update test

--- cypress/integration/user_page_spec.js
+++ cypress/integration/user_page_spec.js
@@ -2,19 +2,29 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 });
 
-describe('user info page e2e tests', () => {
-  it('page loads when not logged in', () => {
-    cy.visit('/omasivu');
-    cy.contains('Kirjaudu sisään ensin');
-  });
+let auth;
 
+before(function fetchUser () {
+  cy.request('POST', 'http://localhost:5000/api/login', {
+    username: 'usernametest',
+    password: 'passwordtest',
+  })
+  .its('body')
+  .then((res) => {
+    auth = res.auth;
+  });
+});
+
+beforeEach(function setUser () {
+  cy.visit('/omasivu', {
+    onBeforeLoad (win) {
+      win.localStorage.setItem('auth', auth);
+    },
+  });
+});
+
+describe('user info page e2e tests', () => {
   it('page loads when logged in', () => {
-    cy.visit('/kirjaudu');
-    cy.get('[data-testid=username]').type('usernametest');
-    cy.get('[data-testid=password]').type('passwordtest');
-    cy.get('[data-testid=kirjaudu]').click();
-    cy.wait(3000);
-    cy.visit('/omasivu');
     cy.contains('Tallenna muutokset');
     cy.contains('Sukellukset kartalla');
     cy.contains('Ilmoitetut hylyt');
@@ -22,29 +32,24 @@ describe('user info page e2e tests', () => {
   });
 
   it('user can change name', () => {
-    cy.visit('/kirjaudu');
-    cy.get('[data-testid=username]').clear().type('usernametest');
-    cy.get('[data-testid=password]').clear().type('passwordtest');
-    cy.get('[data-testid=kirjaudu]').click();
-    cy.wait(3000);
-    cy.visit('/omasivu');
-    cy.get('[data-testid=name]').type('cypress tester')
+    cy.get('[data-testid=name]').clear().type('cypress tester')
     cy.get('[data-testid=save]').click();
     cy.contains('Tiedot tallennettu onnistuneesti');
+    cy.reload(true);
+    cy.wait(1000);
+    cy.get('[data-testid=name]').should('have.value', 'cypress tester');
   });
 
   it('user can phone or email', () => {
-    cy.visit('/kirjaudu');
-    cy.get('[data-testid=username]').clear().type('usernametest');
-    cy.get('[data-testid=password]').clear().type('passwordtest');
-    cy.get('[data-testid=kirjaudu]').click();
-    cy.wait(3000);
-    cy.visit('/omasivu');
     cy.get('[data-testid=phone]').clear().type('1234567890')
     cy.get('[data-testid=save]').click();
     cy.contains('Tiedot tallennettu onnistuneesti');
     cy.get('[data-testid=email]').clear().type('cypress@tester.com')
     cy.get('[data-testid=save]').click();
     cy.contains('Tiedot tallennettu onnistuneesti');
+    cy.reload(true);
+    cy.wait(1000);
+    cy.get('[data-testid=email]').should('have.value', 'cypress@tester.com');
+    cy.get('[data-testid=phone]').should('have.value', '1234567890');
   });
 });
