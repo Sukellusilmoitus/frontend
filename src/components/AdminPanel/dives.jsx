cc3ae494ff2c218ce11092331924984dbcd186db
Shmuli02 <samuli.kaleva@gmail.com>
2022-04-26 14:59:22 +0300

    fix adminpanel dives date

--- src/components/AdminPanel/dives.jsx
+++ src/components/AdminPanel/dives.jsx
@@ -10,7 +10,6 @@ import {
   EditButton,
   UrlField,
   FunctionField,
-  DateField,
   BooleanField,
 } from 'react-admin';
 
@@ -21,7 +20,7 @@ function DiveListView(props) {
         <TextField source="id" />
         <TextField source="diver_name" label="Sukeltaja" />
         <FunctionField source="target_name" label="Hylky" render={(dive) => <UrlField source="target_name" href={`/hylyt/${dive.target_id}`} />} />
-        <DateField source="created_at" label="Päiväys" />
+        <TextField source="divedate" label="Päiväys" />
         <BooleanField source="location_correct" label="Sijainti oikein" />
         <TextField source="new_x_coordinate" label="Uusi x koordinaatti" />
         <TextField source="new_y_coordinate" label="Uusi y koordinaatti" />
@@ -36,14 +35,14 @@ function DiveListEdit(props) {
   return (
     <Edit {...props}>
       <SimpleForm>
-        <TextInput source="diver" />
-        <TextInput source="target" />
-        <TextInput source="created_at" />
-        <TextInput source="location_correct" />
-        <TextInput source="new_x_coordinate" />
-        <TextInput source="new_y_coordinate" />
-        <TextInput source="change_text" />
-        <TextInput source="miscellaneous" />
+        <TextInput source="diver" label="Sukeltaja" />
+        <TextInput source="target" label="Hylky" />
+        <TextInput source="divedate" label="Päiväys" />
+        <TextInput source="location_correct" label="Sijainti oikein" />
+        <TextInput source="new_x_coordinate" label="Uusi x koordinaatti" />
+        <TextInput source="new_y_coordinate" label="Uusi y koordinaatti" />
+        <TextInput source="change_text" label="Muutokset" />
+        <TextInput source="miscellaneous" label="Lisätietoja" />
       </SimpleForm>
     </Edit>
   );
