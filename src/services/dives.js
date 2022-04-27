b1b4b1c6b8dbd3f3e816c059317df7a98185811e
jpasikainen <asikainen.juhapekka@gmail.com>
2022-04-27 14:39:58 +0300

    Add token to request

--- src/services/dives.js
+++ src/services/dives.js
@@ -4,7 +4,10 @@ import REACT_APP_SERVER_URL from '../util/config';
 const baseUrl = REACT_APP_SERVER_URL;
 
 const getAll = async () => {
-  const response = await axios.get(`${baseUrl}/api/dives`);
+  const headers = {
+    'X-ACCESS-TOKEN': localStorage.getItem('auth'),
+  };
+  const response = await axios.get(`${baseUrl}/api/dives`, { headers });
   return response.data;
 };
 
