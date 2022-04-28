1b8880bd535dd6a8a805bdeeb8706a1b8a66e959
synesteesia <mikko.ahokas@hotmail.com>
2022-04-28 16:37:41 +0300

    jestFix

--- src/components/Navigation.test.jsx
+++ src/components/Navigation.test.jsx
@@ -20,7 +20,7 @@ describe('navbar tests', () => {
     expect(links[3]).toHaveTextContent('Uusi hylky')
     expect(links[3].getAttribute('href')).toBe('/uusi')
 
-    expect(links[4]).toHaveTextContent('Anna palautetta')
+    expect(links[4]).toHaveTextContent('Info ja palaute')
     expect(links[4].getAttribute('href')).toBe('/palaute')
 
     expect(links[5]).toHaveTextContent('Kirjaudu')
