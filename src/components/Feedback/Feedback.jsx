40289a26d9066cd011aecc21c04b2c2f4ffa4734
synesteesia <43009268+synesteesia@users.noreply.github.com>
2022-04-29 16:25:08 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -14,7 +14,6 @@ function Feedback() {
       feedback_giver_email: feedback.email,
       feedback_giver_phone: feedback.phone,
     };
-    console.log('hello');
     try {
       feedbackService.create(newFeedback);
       setMessage('Palaute lähetetty');
